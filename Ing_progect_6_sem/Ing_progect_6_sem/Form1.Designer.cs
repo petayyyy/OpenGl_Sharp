1496a722abbd29d8e96890147f51ceda07e53449
petayyyy <ilyaholodilov19072002@gmail.com>
2023-05-17 10:25:00 +0300

    kkkkkkkkk

---
+++ Ing_progect_6_sem/Ing_progect_6_sem/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ing_progect_6_sem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Main_picture = new System.Windows.Forms.PictureBox();
+            this.Open_but = new System.Windows.Forms.Button();
+            this.Start_cam_but = new System.Windows.Forms.Button();
+            this.Restart_but = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Main_picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MP4|*.mp4|PNG|*.png|JPG|*.jpg|BMP|*.bmp|AVI|*.avi";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main_picture
+            // 
+            this.Main_picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Main_picture.Location = new System.Drawing.Point(12, 12);
+            this.Main_picture.Name = "Main_picture";
+            this.Main_picture.Size = new System.Drawing.Size(1280, 960);
+            this.Main_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Main_picture.TabIndex = 0;
+            this.Main_picture.TabStop = false;
+            // 
+            // Open_but
+            // 
+            this.Open_but.BackColor = System.Drawing.Color.White;
+            this.Open_but.Location = new System.Drawing.Point(1320, 12);
+            this.Open_but.Name = "Open_but";
+            this.Open_but.Size = new System.Drawing.Size(120, 80);
+            this.Open_but.TabIndex = 1;
+            this.Open_but.Text = "Open video";
+            this.Open_but.UseVisualStyleBackColor = false;
+            this.Open_but.Click += new System.EventHandler(this.Open_but_Click);
+            // 
+            // Start_cam_but
+            // 
+            this.Start_cam_but.BackColor = System.Drawing.Color.White;
+            this.Start_cam_but.Location = new System.Drawing.Point(1320, 107);
+            this.Start_cam_but.Name = "Start_cam_but";
+            this.Start_cam_but.Size = new System.Drawing.Size(120, 80);
+            this.Start_cam_but.TabIndex = 2;
+            this.Start_cam_but.Text = "Start read camera";
+            this.Start_cam_but.UseVisualStyleBackColor = false;
+            this.Start_cam_but.Click += new System.EventHandler(this.Start_cam_but_Click);
+            // 
+            // Restart_but
+            // 
+            this.Restart_but.BackColor = System.Drawing.Color.White;
+            this.Restart_but.Location = new System.Drawing.Point(1320, 207);
+            this.Restart_but.Name = "Restart_but";
+            this.Restart_but.Size = new System.Drawing.Size(120, 80);
+            this.Restart_but.TabIndex = 3;
+            this.Restart_but.Text = "Refresh";
+            this.Restart_but.UseVisualStyleBackColor = false;
+            this.Restart_but.Click += new System.EventHandler(this.Restart_but_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1654, 992);
+            this.Controls.Add(this.Restart_but);
+            this.Controls.Add(this.Start_cam_but);
+            this.Controls.Add(this.Open_but);
+            this.Controls.Add(this.Main_picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Main_picture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox Main_picture;
+        private System.Windows.Forms.Button Open_but;
+        private System.Windows.Forms.Button Start_cam_but;
+        private System.Windows.Forms.Button Restart_but;
+    }
+}
+
---
+++ Ing_progect_6_sem/Ing_progect_6_sem/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace Ing_progect_6_sem
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture video_capture;
+        Mat input_flow = new Mat();
+        Mat gray_flow = new Mat();
+        //Mat cam;
+        Mat cam_matrix = new Mat(3, 3, MatType.CV_32FC1, new float[,] { { 1.35662728e+03f, 0.0f, 2.91998600e+02f }, { 0.0f, 1.37532524e+03f, 2.25387379e+02f }, { 0.0f, 0.0f, 1.0f } });
+        // Dist coef
+        Mat dis_coef = new Mat(14, 1, MatType.CV_32FC1, new float[] { -1.32575155e+00f, -7.35188200e+00f, 4.29782934e-02f, 7.66436446e-02f, 5.18928027e+01f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f, 0.00000000e+00f });
+        bool is_video = false;
+        bool is_cam = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Main_picture.Image = new Bitmap(640, 480);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    if (video_capture.IsOpened() && (is_cam || is_video))
+                    {
+                        video_capture.Read(gray_flow);
+                        //MessageBox.Show(gray_flow.Height.ToString());
+                        Main_picture.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(gray_flow);
+                        Main_picture.Refresh();
+                    }
+                    //MessageBox.Show(video_capture.IsOpened().ToString());
+                }
+                catch
+                {
+                    if (is_video)
+                    {
+                        Restart_but.Text = "Restart video";
+                    }
+                }
+                //input_flow = input_flow.Resize(new OpenCvSharp.Size(640, 480));
+                // BGR to GRAY
+                Cv2.CvtColor(input_flow, gray_flow, ColorConversionCodes.BGR2GRAY);
+                Main_picture.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(input_flow);
+                Main_picture.Refresh();
+            }
+            catch
+            {
+            }
+        }
+        private void Open_but_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                //if (openFileDialog1.FileName.Contains(".mp4") || openFileDialog1.FileName.Contains(".avi"))
+                //{
+                //is_video = true;
+                //video_capture.Open(openFileDialog1.FileName);
+                //video_capture.Open("video_1.avi");
+                //MessageBox.Show(openFileDialog1.FileName);
+
+                video_capture = new VideoCapture(openFileDialog1.FileName);
+                Mat m = new Mat();
+                video_capture.Read(m);
+                MessageBox.Show(video_capture.IsOpened().ToString());
+                Main_picture.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(m);
+                Main_picture.Refresh();
+
+
+
+
+                //string path = AppDomain.CurrentDomain.BaseDirectory;
+                //    string fullpath = System.IO.Path.Combine(path, "video_1.avi");
+                //    //video_capture = VideoCapture.FromFile(fullpath);
+                //    video_capture.Open("video_1.avi");
+                //}
+                //else
+                //{
+                //    is_video = false;
+                //    input_flow = new Mat(openFileDialog1.FileName);
+                //    Main_picture.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(input_flow);
+                //}
+                //timer1.Start();
+                //Graphics graphics = Graphics.FromImage(Main_picture.Image);
+                //graphics.FillRectangle(Brushes.Black, new Rectangle(0, 0, Main_picture.Image.Width, Main_picture.Image.Height));
+                //Main_picture.Refresh();
+                //    }
+                //    else MessageBox.Show("Error, you don't take any file.");
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+                //    MessageBox.Show("Error, your file have incorrect type. You must take .png, .jpg or .bmp.");
+                //}
+            }
+        }
+
+        private void Restart_but_Click(object sender, EventArgs e)
+        {
+            if (Restart_but.Text == "Restart video")
+            {
+                video_capture.Open(openFileDialog1.FileName);
+                Restart_but.Text = "Refresh";
+            }
+            else
+            {
+                timer1.Stop();
+                is_cam = false;
+                is_video = false;
+                Start_cam_but.Enabled = true;
+                Open_but.Enabled = true;
+                Graphics graphics = Graphics.FromImage(Main_picture.Image);
+                graphics.FillRectangle(Brushes.Pink, new Rectangle(0, 0, Main_picture.Image.Width, Main_picture.Image.Height));
+                Main_picture.Refresh();
+                video_capture.Release();
+            }
+        }
+
+        private void Start_cam_but_Click(object sender, EventArgs e)
+        {
+            is_cam = true;
+            Open_but.Enabled = false;
+            timer1.Start();
+            video_capture = new VideoCapture(0);
+            video_capture.Open(0);
+        }
+    }
+
+}
+
+
+
